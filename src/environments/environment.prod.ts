70b2638277e91f152e259c66d7e2b0e5f68adec5
mudlee <contact@mudlee.hu>
2022-06-10 11:12:09 +0200

    Finalizing First Release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   endpoints: {
     defaultTenant: '/assets/b72735af-a7ec-4d22-82d0-b721c7aa625d',
     currentTenant: '$DOCKER_currentTenant',
-    // currentTenant: '/assets/695c4fb2-5ed0-474f-bf51-f982a8d2c2da',
     webService: '$DOCKER_webService',
   },
   hydraledgerNetwork: '$DOCKER_hydraledgerNetwork',
